6aa70db64dd074830a04649b11cf9112553eca1b
Sergei panov <sergeipanov@gmail.com>
2025-03-26 22:08:56 -0400

    Update all files: Fix audio file loading and note naming conventions

--- gameData.js
+++ gameData.js
@@ -370,7 +370,7 @@ const gameData = {
     },
     level11: {
       name: "Level 11: Ultimate Challenge",
-      description: "Test your mastery with increased speed requirements and higher accuracy demands",
+      description: "Test your knowledge of all notes in first position on the violin",
       notes: ["G3","Ab3", "A3", "Bb3", "B3", "C4", "C#4", "Db4", "D4", "Eb4", "E4", "F4", "F#4", "G4", "G#4", "Ab4", "A4", "Bb4", "B4", "C5", "C#5", "D5", "D#5", "Eb5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "Bb5", "B5"],
       unlockRequirement: 100,
       sharps: true,
